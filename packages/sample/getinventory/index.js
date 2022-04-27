5444bd193b02b861d880d68113c3f35cdba63d78
Davi DeBarros <ddebarros@digitalocean.com>
2022-04-27 10:25:45 -0400

    test -1

--- packages/sample/getinventory/index.js
+++ packages/sample/getinventory/index.js
@@ -1,46 +1,20 @@
-// This works
-// function main() {
-//     let greeting = "What's up"
-//     return {
-//         "body": greeting
-//         }
-// }
-
-require ('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
-const fs = require('fs');
-const path = require('path');
+const connect = require('./mongodb-client');
 
 async function main() {
-    const DB_CERT = process.env['CA_CERT'].replace(/\\n/g, '\n');
-    const DATABASE_URL = process.env['DATABASE_URL'];
-    const uri = DATABASE_URL
+  try {
+    const client = await connect()
+    const inventory = await client.db("admin").collection("inventory").find().toArray();
+    console.log(inventory);
+    return {
+      "body": inventory
+    }
 
-    const tlsCAFilePath = path.resolve(__dirname, 'certificate.pem');
-    fs.writeFileSync(tlsCAFilePath, DB_CERT, { encoding: 'utf8' });
-
-    let client = new MongoClient(uri, {
-        tls: true,
-        tlsCAFile: tlsCAFilePath,
-        tlsInsecure: true
-    });
-
-    try {
-        await client.connect();
-        // await listDatabases(client);
-        const inventory = await client.db("admin").collection("inventory").find().toArray();
-        console.log(inventory);
-        return {
-            "body": inventory
-            }
-
-        } catch {
-            console.error();
-        } finally {
-            await client.close();
-        }
+  } catch {
+    console.error();
+  }
 }
 
+exports.main = main;
 // main().catch(console.error);
 
 // async function listDatabases(client) {
